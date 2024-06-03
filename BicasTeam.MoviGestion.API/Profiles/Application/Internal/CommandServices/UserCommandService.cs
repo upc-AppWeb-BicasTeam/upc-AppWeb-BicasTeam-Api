7b06c81e4f7e85483c311a868ea3ff5476a3a2ed
pierotm <pierotarazona822@gmail.com>
2024-06-03 01:24:22 -0500

    feat(profiles): implemented query service and command for user aggregate.

---
+++ BicasTeam.MoviGestion.API/Profiles/Application/Internal/CommandServices/UserCommandService.cs
@@ -0,0 +1,26 @@
+﻿using BicasTeam.MoviGestion.API.Profiles.Domain.Model.Aggregates;
+using BicasTeam.MoviGestion.API.Profiles.Domain.Model.Commands;
+using BicasTeam.MoviGestion.API.Profiles.Domain.Repositories;
+using BicasTeam.MoviGestion.API.Profiles.Domain.Services;
+using BicasTeam.MoviGestion.API.Shared.Domain.Repositories;
+
+namespace BicasTeam.MoviGestion.API.Profiles.Application.Internal.CommandServices;
+
+public class UserCommandService(IUserRepository userRepository, IUnitOfWork unitOfWork)
+    : IUserCommandService
+{
+    public async Task<User?> Handle(CreateUserCommand command)
+    {
+        var user = new User(command);
+        try
+        {
+            await userRepository.AddAsync(user);
+            await unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+        return user;
+    }
+}
