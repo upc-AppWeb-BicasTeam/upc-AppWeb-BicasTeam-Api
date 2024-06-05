56215a8ef014fdab57272dec163cd4b918041742
pierotm <pierotarazona822@gmail.com>
2024-06-05 18:26:46 -0500

    fix:  deleted ShipmentsResource

--- BicasTeam.MoviGestion.API/Profiles/Interfaces/REST/Resources/ShipmentResource.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace BicasTeam.MoviGestion.API.Profiles.Interfaces.REST.Resources;
-
-public record ShipmentResource (int UserId, string Destiny, string Description, DateTimeinfo DateTime, string Status);
-
-public record DateTimeinfo(DateTime Date, TimeSpan Time);
